a1963170ff031032d802932463f00c13b3c57c4d
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-05-08 11:16:19 -0300

    Initial commit of Iridium Bank API

---
+++ src/main/java/com/iridium/bankApi/IridiumBankApplication.java
@@ -0,0 +1,13 @@
+package com.iridium.bankApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IridiumBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IridiumBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iridium/bankApi/IridiumBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iridium.bankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IridiumBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
